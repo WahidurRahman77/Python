ba4c23ef1b8c476a0f250fb3cd622229a3f677d0
Wahidur Rahman <wrrahman456@gmail.com>
2025-04-26 18:50:00 +0600

    Add files via upload

---
+++ St. Chroma.py
@@ -0,0 +1,22 @@
+def main():
+    import sys
+    input = sys.stdin.read 
+    data = input().split()
+    t = int(data[0])
+    index = 1 
+    for _ in range(t):
+        n = int(data[index])
+        x = int(data[index+1])
+        index += 2
+        if x == 0:
+            perm = list(range(1, n)) + [0]
+        elif x == n:
+            perm = list(range(n))
+        else:
+            part1 = list(range(x))
+            part2 = list(range(x+1, n))
+            perm = part1 + part2 + [x]
+        print(' '.join(map(str, perm)))
+    
+if __name__ == "__main__":
+    main()
