da89b7c60dcd30c2a11ee11e971b211b9e37a792
Wahidur Rahman <wrrahman456@gmail.com>
2025-05-24 11:29:40 +0600

    Add files via upload

---
+++ Notes/Notes.py
@@ -0,0 +1,39 @@
+#one line comment
+
+'''
+multiple
+line
+comment
+'''
+print (123)
+print ("\"Wahidur Rahman\"")
+
+#Variables and Data Types
+name = ("Wahidur Rahman")
+age = 18
+print ("Our new student name is " + name)
+print(name + " is " , age , " years old.")
+print("At the age of ", age , "he learned python.")
+print(name + " lives in canad")
+
+# Basic Numerical Operations
+a = 20
+b= 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+
+
+#Getting User Input
+name = input("Enter your name: ")
+print(name)
+
+#Type Casting
+num1 = int(input("Enter your first number: "))
+num2 = input("Enter your second number: ")
+sum = num1 + int(num2)
+print(sum)
