a39045d99774ce9ba73c1d4fe038e77e66254ddf
Wahidur Rahman <wrrahman456@gmail.com>
2025-05-25 18:12:02 +0600

    Update Notes.py

--- Notes/Notes.py
+++ Notes/Notes.py
@@ -37,3 +37,89 @@ num1 = int(input("Enter your first number: "))
 num2 = input("Enter your second number: ")
 sum = num1 + int(num2)
 print(sum)
+
+#Math related Library functions
+
+from math import *
+print(max(10, 20))
+print(min(10, 20))
+print(abs(-4))
+print(pow(2, 3))
+print(sqrt(25))
+print(round(3.8))
+print(floor(3.8))
+print(ceil(3.8))
+
+#Formatted String | Type function
+num = 3.8
+print(type(num))
+
+num1 = 2
+num2 = 5
+print(f"{num1} + {num2} = {num1 + num2}")
+
+print("Wahidur Rahman", end=" ")
+print("123456789")
+
+#if, else statement
+if (3 > 4):
+    print(3)
+else:
+    print(4)
+
+#elif
+n =5
+if(n>3):
+    print(3)
+elif(n>4):
+    print(4)
+else:
+    print(5)
+
+#Ternary Operator
+num1 = 2
+num2 = 4
+print(num1 if num1<num2 else num2)
+
+#Logical operator
+if(num1<num2 and num2< 5):
+    print(num1)
+elif(num1<num2 or num2>2):
+    print(num2)
+
+#List
+list = ["c", "c++", "java", "python", "c#", "c++", "java", "python"]
+print(list)
+print(list[0])
+print(list[2:])
+print(list[-1])
+
+print("python" in list)
+
+print("nodejs" not in list)
+
+print(len(list))
+
+list.append("TOC")
+list.insert(2, "OS")
+list.remove("java")
+
+list.sort()
+print (list)
+list.reverse()
+list.pop()
+print(list)
+list2 = list.copy()
+list.clear()
+print(list2)
+print(list2.index("java"))
+print(list2.count("python"))
+
+# Range function
+num = list(range(5, 10))
+print(num)
+
+num = list(range(5, 20, 2))
+print(num)
+
+
